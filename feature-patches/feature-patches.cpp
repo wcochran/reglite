455484c325a056586de340f42d022022e9b20ea2
Wayne O. Cochran <wayne.cochran@gmail.com>
2022-11-17 11:15:33 -0800

    Now output corresponding JSON file with patch / feature info (no 3D point data yet).

--- feature-patches/feature-patches.cpp
+++ feature-patches/feature-patches.cpp
@@ -16,17 +16,38 @@ using nlohmann::json;
 using namespace cv;
 using namespace rectpack2D;
 
+struct PatchInfo {
+    Eigen::Vector2f keypoint;         // position of feature in original image
+    cv::Rect rect;                    // location and size of patch in original image
+    rectpack2D::rect_xywh packedRect; // location and size of patch in packed rectangle image
+    Eigen::Vector3f point3D;          // corresponding 3D location (unused at the moment)
+    PatchInfo(float x, float y,
+              cv::Rect rect,
+              rectpack2D::rect_xywh patch,
+              float X = 0, float Y = 0, float Z = 0)
+    : keypoint{x,y}, rect{rect}, packedRect{patch}, point3D{X,Y,Z} {}
+};
+
+void to_json(json& j, const PatchInfo& patch) {
+    j["keypoint"] = {patch.keypoint.x(), patch.keypoint.y()};
+    j["patch_pos"] = {patch.rect.x, patch.rect.y};
+    j["patch_size"] = {patch.rect.width, patch.rect.height};
+    j["packed_pos"] = {patch.packedRect.x, patch.packedRect.y};
+    j["patch_point3D"] = {patch.point3D.x(), patch.point3D.y(), patch.point3D.z()};
+}
+
 int main(int argc, char *argv[]) {
 
-    if (argc != 4) {
+    if (argc != 5) {
         std::cerr << "usage: " << argv[0]
-                  << " image_corr.json image.jpg patches.jpg\n";
+                  << " image_corr.json image.jpg patches.jpg patches.json\n";
         exit(1);
     }
 
     std::string regdata_json_path(argv[1]);
     std::string image_path(argv[2]);
-    std::string patches_path(argv[3]);
+    std::string patches_image_path(argv[3]);
+    std::string patches_metadata_path(argv[4]);
 
     Mat image = imread(image_path, IMREAD_COLOR);
     if (image.empty()) {
@@ -34,6 +55,7 @@ int main(int argc, char *argv[]) {
                   << image_path << "'!\n";
         exit(-1);
     }
+    Rect imageRect(0, 0, image.cols, image.rows);
     
     std::vector<Point2f> closestPoints;
     std::vector<std::array<float,4>> closestShapes;
@@ -77,6 +99,7 @@ int main(int argc, char *argv[]) {
     assert(closestPoints.size() == closestShapes.size());
 
     std::vector<Mat> patches;
+    std::vector<PatchInfo> patchMetadata;
 
 	constexpr bool allow_flip = false;
 	const auto runtime_flipping_mode = flipping_option::DISABLED;
@@ -85,7 +108,7 @@ int main(int argc, char *argv[]) {
 	std::vector<rect_type> rectangles;
 
     constexpr int padding = 4;
-    constexpr double bboxScale = 2.0;
+    constexpr double bboxScale = 1.414;
 
     for (size_t i = 0; i < closestPoints.size(); i++) {
         const Point2f& p = closestPoints[i];
@@ -106,14 +129,20 @@ int main(int argc, char *argv[]) {
         const double bboxTop = p.y - bboxHeight;
         const double bboxBottom = p.y + bboxHeight;
         // Note: I think x0,y0 can be non-integral (?)
-        const int x0 = int(std::round(bboxLeft));
-        const int y0 = int(std::round(bboxTop));
-        const int W = int(std::round(bboxRight)) - x0;
-        const int H = int(std::round(bboxBottom)) - y0;
+        const int x0 = int(std::floor(bboxLeft));
+        const int y0 = int(std::floor(bboxTop));
+        const int W = int(std::ceil(bboxRight)) - x0;
+        const int H = int(std::ceil(bboxBottom)) - y0;
+        if (W < 2 || H < 2) continue;
         Rect rect(x0, y0, W, H);
+        const bool is_inside = (rect & imageRect) == rect;
+        if (!is_inside) continue;
         Mat patch = image(rect);
         patches.emplace_back(std::move(patch));
-        rectangles.emplace_back(rect_xywh(0,0,W+padding,H+padding));
+        rect_xywh packedRect(0,0,W+padding,H+padding);
+        rectangles.emplace_back(packedRect);
+        PatchInfo info{p.x, p.y, rect, packedRect};
+        patchMetadata.emplace_back(info);
     }
 
 	const auto max_side = 1000;
@@ -146,19 +175,40 @@ int main(int argc, char *argv[]) {
         rectToIndex.insert({std::make_pair(rect.w,rect.h),i});
     }
 
+    assert(patches.size() == patchMetadata.size());
+
     Mat packedPatches(result_size.h, result_size.w, CV_8UC3, Scalar(0, 0, 0));
-    for (auto&& patch : patches) {
+    for  (size_t i = 0; i < patches.size(); i++) {
+        auto& patch = patches[i];
+        auto& patchInfo = patchMetadata[i];
         const auto wh = std::make_pair(patch.cols + padding, patch.rows + padding);
         auto iter = rectToIndex.find(wh);
         assert(iter != rectToIndex.end());
         const size_t index = iter->second;
         const auto rect = rectangles[index];
+//        std::cout << "rect: " << rect.x << "," << rect.y << ":" << rect.w << "x" << rect.h << std::endl;
         Rect roi(rect.x,rect.y,rect.w - padding,rect.h - padding);
         patch.copyTo(packedPatches(roi));
         rectToIndex.erase(iter);
+        patchInfo.packedRect = rect;
     }
 
-    imwrite(patches_path, packedPatches);
-    
+    imwrite(patches_image_path, packedPatches);
+
+    json patchData = json::array();
+    for (auto&& patchInfo : patchMetadata) {
+        json j = json::object();
+        to_json(j, patchInfo);
+        patchData.push_back(j);
+    }
+
+    std::ofstream js(patches_metadata_path);
+    if (!js.is_open()) {
+        std::cerr << "Unable to open '" << patches_metadata_path << "' for writing!\n";
+        exit(-1);
+    }
+    js << patchData;
+    js.close();
+
     return 0;
 }
